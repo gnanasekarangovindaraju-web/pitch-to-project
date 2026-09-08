87140796f5b16a22cb0fc5cdf0b7a0a120597a0e
gnanasekarangovindaraju-web <gnanasekaran.govindaraju@hurix.com>
2026-09-08 19:15:14 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- streamlit_app.py
+++ streamlit_app.py
@@ -613,7 +613,7 @@ def check_password():
 
                 valid_user = st.secrets.get(
                     "APP_USER",
-                    "adin"
+                    "admin"
                 )
 
                 valid_password = st.secrets.get(
